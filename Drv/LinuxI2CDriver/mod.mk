eedb91f98fa95e0740e1d5bdd988a354f48373af
Sterling Peet <sterling.peet@ae.gatech.edu>
2019-06-04 18:55:18 -0400

    fixes to get the code to compile

--- Drv/LinuxI2CDriver/mod.mk
+++ Drv/LinuxI2CDriver/mod.mk
@@ -1,25 +1,11 @@
-s#
+#
 #   Copyright 2004-2008, by the California Institute of Technology.
 #   ALL RIGHTS RESERVED. United States Government Sponsorship
 #   acknowledged.
 #
 #
 
-SRC = LinuxI2CDriverComponentAi.xml LinuxI2CDriverComponentImplCommon.cpp
+SRC = LinuxI2CDriverComponentAi.xml
 
-SRC_SDFLIGHT = LinuxI2CDriverComponentImpl.cpp
 
-SRC_LINUX = LinuxI2CDriverComponentImpl.cpp
-
-SRC_CYGWIN = LinuxI2CDriverComponentImpl.cpp
-
-SRC_DARWIN = LinuxI2CDriverComponentImplStub.cpp
-
-SRC_RASPIAN = LinuxI2CDriverComponentImpl.cpp
-
-SRC_LINUXRT = LinuxI2CDriverComponentImpl.cpp
-
-HDR = LinuxI2CDriverComponentImpl.hpp
-
-SUBDIRS = test
 
