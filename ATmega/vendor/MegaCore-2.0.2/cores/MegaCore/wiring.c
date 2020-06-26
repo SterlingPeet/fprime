ab538bf7396c2d706d4967504a71fb10fe6c15b3
Nathan Cheek <nathan@nathancheek.com>
2020-06-26 00:32:32 -0500

    Use more accurate MICROSECONDS_PER_TIMER0_OVERFLOW with 7.3728MHZ clock

--- ATmega/vendor/MegaCore-2.0.2/cores/MegaCore/wiring.c
+++ ATmega/vendor/MegaCore-2.0.2/cores/MegaCore/wiring.c
@@ -27,7 +27,11 @@
 // 24MHz: An overflow happens every 682.67 microseconds ---> 0.04167, so this results in 682 
 // 20MHz: An overflow happens every 819.2 microseconds ---> 0,05 (time of a cycle in micros) * 64 (timer0 tick) * 256 (every 256 ticks timer0 overflows), so this results in 819
 // 16MHz: An overflow happens every 1024 microseconds
+#if F_CPU == 7372800L
+#define MICROSECONDS_PER_TIMER0_OVERFLOW 2222 // Real value is 2222.22... but this is more precise than clockCyclesToMicroseconds() which truncates 7.3728 -> 7, leading to 5% speedup error
+#else
 #define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
+#endif
 
 // the whole number of milliseconds per timer0 overflow
 // For 20MHz this would be 0 (because of 819)
